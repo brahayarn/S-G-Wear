6e3a9e1fd97c5159e6ff14659b55fbefce8d1210
brahayarn <hrabar.yana@student.uzhnu.edu.ua>
2024-12-19 17:58:15 +0300

    feat: add link to product detail page with item id

--- code/src/components/CardItems/CardItemsMan.jsx
+++ code/src/components/CardItems/CardItemsMan.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import{clothesman} from '../../data/clothesman.js'
 import "../../styles/block/_card.scss";
+import { Link } from 'react-router-dom';
 
 const CardItemsMan = () => {
   return (
     <div className="container__card">
-            {clothesman.map((item, index) => (
-                <div className="card" key={index}>
-                    <img src={item.image} alt="clothes" className="card__image" />
-                    <div className="card__info">
-                        <h3 className="card__title">{item.name}</h3>
-                        <p className="card__price">${item.price}</p>
-                    </div>
-                </div>
-            )
-            )}
+      {clothesman.map((item, index) => (
+        <div className="card" key={index}>
+          {/* Передаємо item.id для маршруту */}
+          <Link to={`/new-collection/${item.id}`}>
+            <img src={item.image} alt={item.name} className="card__image" />
+          </Link>
+          <div className="card__info">
+            <h3 className="card__title">{item.name}</h3>
+            <p className="card__price">${item.price}</p>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default CardItemsMan
--- code/src/components/CardItems/CardItemsWoman.jsx
+++ code/src/components/CardItems/CardItemsWoman.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import{clotheswoman} from '../../data/clotheswoman.js'
 import "../../styles/block/_card.scss";
+import { Link } from 'react-router-dom';
 
 const CardItemsMan = () => {
   return (
     <div className="container__card">
-            {clotheswoman.map((item, index) => (
-                <div className="card" key={index}>
-                    <img src={item.image} alt="clothes" className="card__image" />
-                    <div className="card__info">
-                        <h3 className="card__title">{item.name}</h3>
-                        <p className="card__price">${item.price}</p>
-                    </div>
-                </div>
-            ))}
+      {clotheswoman.map((item, index) => (
+        <div className="card" key={index}>
+          <Link to={`/new-collection/${item.id}`}>
+            <img src={item.image} alt={item.name} className="card__image" />
+          </Link>
+          <div className="card__info">
+            <h3 className="card__title">{item.name}</h3>
+            <p className="card__price">${item.price}</p>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 export default CardItemsMan
